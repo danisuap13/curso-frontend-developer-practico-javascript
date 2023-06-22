1cd7443da8c5861bc08a3062537b9337dc90a29d
Daniel Suarez <est.daniel.suarez1@unimilitar.edu.co>
2023-06-22 13:43:46 -0500

    Inclusión JavaScript al detailContainer

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-secondary-close')
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCart = document.querySelector('.product-detail');
+const productDetailContainer = document.querySelector('.product-detail-secondary');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu );
 menuCarritoIcon.addEventListener('click', toggleCarritoMenu);
+productDetailCloseIcon.addEventListener('click',closeProductDetailAside);
 
 function toggleDesktopMenu(){
     const isShoppingCartClosed = !shoppingCart.classList.contains('inactive');
@@ -20,13 +23,16 @@ function toggleMobileMenu(){
     const isShoppingCartClosed = !shoppingCart.classList.contains('inactive');
     if(isShoppingCartClosed) shoppingCart.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+    closeProductDetailAside();
 }
 
 function toggleCarritoMenu(){
     const isMobileMenuClosed = !mobileMenu.classList.contains('inactive');
     const isDesktopMenuClosed = !desktopMenu.classList.contains('inactive');
+    const isProductDetailAsideClosed = !productDetailContainer.classList.contains('inactive');
     if(isMobileMenuClosed) mobileMenu.classList.add('inactive');
     if(isDesktopMenuClosed) desktopMenu.classList.add('inactive');
+    if(isProductDetailAsideClosed) productDetailContainer.classList.add('inactive');
     shoppingCart.classList.toggle('inactive');
 }
 
@@ -38,6 +44,7 @@ function renderProducts(arr){
 
     const img = document.createElement('img');
     img.setAttribute('src', product.image);
+    img.addEventListener('click', openProductDetailAside);
     
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
@@ -62,6 +69,15 @@ function renderProducts(arr){
     }   
 }
 
+function openProductDetailAside(){
+    const isShoppingCartClosed = !shoppingCart.classList.contains('inactive');
+    if(isShoppingCartClosed) shoppingCart.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive')
+}
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive');
+}
 const productList = [];
 productList.push ({
     name: 'Bike',
@@ -83,25 +99,6 @@ productList.push ({
     price: 20,
     image: "https://media.istockphoto.com/id/500062008/es/foto/ni%C3%B1o-sentado-en-la-rampa-hacia-abajo-en-monopat%C3%ADn.jpg?s=612x612&w=is&k=20&c=-LTCYPfTPQnrqrFq8KuH7o2Po3SpHg1oD1gI8sFrYfw=",
 });
-productList.push ({
-    name: 'Bike',
-    price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-});
-productList.push ({
-    name: 'Screen',
-    price: 120,
-    image: "https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-});
-productList.push ({
-    name: 'Computer',
-    price: 300,
-    image: "https://media.istockphoto.com/id/1389603578/es/foto/pantalla-en-blanco-de-la-computadora-port%C3%A1til-en-la-mesa-de-madera-con-fondo-interior-de.jpg?s=612x612&w=is&k=20&c=It8bdNPAR-pIBEzDvWO4WwDpV9ptcjDGde2nTvl4b6g=",
-});
-productList.push ({
-    name: 'Skate',
-    price: 20,
-    image: "https://media.istockphoto.com/id/500062008/es/foto/ni%C3%B1o-sentado-en-la-rampa-hacia-abajo-en-monopat%C3%ADn.jpg?s=612x612&w=is&k=20&c=-LTCYPfTPQnrqrFq8KuH7o2Po3SpHg1oD1gI8sFrYfw=",
-});
+
 
 renderProducts(productList);
